07f3d27ac65493f1af0e130081c5cce33d42852e
Hayden Ramm <hayden.r.ramm@gmail.com>
2025-04-23 15:31:19 +0200

    implemented all device methods

---
+++ methods/device_methods.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+from typing import List, Tuple
+
+def load_digicam_image(path:str)->np.ndarray:
+    """Loads image object from .csv given by DigiCam"""
+    #Remove top row and first column, as this is coordinate data
+    img = np.genfromtxt(path, delimiter=',', skiprows=1)
+    img = np.delete(img, 0, axis=1)
+    return img
+
+def bkg_subtraction(raw_arr:np.ndarray, bkg_arr:np.ndarray)->np.ndarray:
+    """Subtracts some background array from some raw data array."""
+    corrected_array = np.subtract(raw_arr, bkg_arr)
+    return corrected_array
+
+def lineout(array:np.ndarray, axis:int)->Tuple[np.ndarray, np.ndarray]:
+    """Sums along given array axis."""
+    if axis + axis == axis:
+        pixels_1D = np.arange(0, array.shape[1], 1)
+    else:
+        pixels_1D = np.arange(0, array.shape[0], 1)
+    lineout = np.sum(array, axis=axis)
+    return lineout, pixels_1D
+
+def arrays_stats(arrays:List[np.ndarray])->Tuple[np.ndarray,np.ndarray]:
+    stack = arrays[0]
+
+    for arr in arrays[1:]:
+        stack = np.stack([stack, arr], axis=0)
+
+    #Array of mean values
+    mean_arr = np.multiply(np.sum(stack, axis=0), 1/len(arrays))
+    #Array of std values
+    std_arr = np.std(stack, axis=0)
+
+    return mean_arr, std_arr
+
+
+def load_scope_voltages(path:str, volt_key="Ampl", skiprows=4):
+    voltages = pd.read_csv(path, skiprows=skiprows)[volt_key]
+    return voltages
+
+def load_scope_times(path:str, time_key = "Time", skiprows = 4):
+    times = pd.read_csv(path, skiprows=skiprows)[time_key]
+    return times
+
+
