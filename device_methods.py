0ae4f2fe0bab0b5b0715e8732f68dfb3f77ade39
Hayden Ramm <hayden.r.ramm@gmail.com>
2025-04-25 15:22:22 +0200

    scrapped device_methods

--- device_methods.py
+++
@@ -1,6 +0,0 @@
-#######################################
-# TO BE MOVED INTO OPERATIONS MANAGER #
-#######################################
-
-def fit_some_gaussian_to_some_image_corrected_or_not_and_plot():
-    pass
