fixed shot averaging

operationsmanager.py
@@ -76,7 +76,7 @@ class ImageManager(OperationsManager):
     def __init__(self, DEVICE_NAME, shot_no, label, shot_data):
         super().__init__(DEVICE_NAME, shot_no, label, shot_data)
     
-    def average_shots(data_list:List[Dict[str, np.ndarray]], shot_nos:List[int]):
+    def average_shots(data_list:List[Dict[str, np.ndarray]]):
         """Performs statistical average (mean) over a supplied list of shots. Note
         that the data list of shots is now a list of dictionaries, which for images
         will look like {"DATA": []}, and for probes will look like {"DATA": {"X":[], "Y":[]}}.
@@ -85,8 +85,11 @@ class ImageManager(OperationsManager):
         ----------
             data_list : List[Dict[str, np.ndarray]]
                 List containing the data which we want to average over. 
-            shot_nos : List[int]
-                List of the shots over which the average has been performed.
+
+        Returns
+        -------
+            mean_arr : np.ndarray
+                The averaged array ALONE (no x or y data)
         
         """
         
@@ -100,12 +103,6 @@ class ImageManager(OperationsManager):
         #PERFORM SUM AND THEN AVERAGE OVER SHOTS (I.E. OVER 0 AXIS)
         sum_arr = np.sum(array_stack, axis=0)
         mean_arr = np.multiply(sum_arr, 1/len(data_list))
-        pixels_x = None
-        pixels_y = None
-
-        plt.imshow(mean_arr)
-        plt.title(f"Averaged Image Over Shots {shot_nos}")
-        plt.show()
 
         return mean_arr
     
@@ -165,8 +162,8 @@ class DigicamImageManager(ImageManager):
 
             thresh = 0.2
             max_intensity = np.max(img)
-            mu = [0, 0]
-            var = [0, 0]
+            mu = [0., 0.]
+            var = [0., 0.]
 
             total_counts = 0
             

run_manager.py
@@ -96,36 +96,66 @@ class RunManager:
         
         # SHOT AVERAGING
         if self.operations["AVERAGE_SHOTS"]:
+            print("Averaging shots ... \n")
+
             averaged_shot_data = {}
             shot_data_list = [data for data in data_dict.values()]
+
+            #GET THE AVERAGED DATA ITSELF
             averaged_shot_data["DATA"] =\
-                self.manager_key[self.input["DEVICE_TYPE"]][self.input["DEVICE_SPECIES"]].average_shots(shot_data_list, shot_nos)
+                self.manager_key[self.input["DEVICE_TYPE"]][self.input["DEVICE_SPECIES"]].average_shots(shot_data_list)
             
-            averaged_shot_data["X"] = np.arange(0, averaged_shot_data["DATA"].shape[1])
-            averaged_shot_data["Y"] = np.arange(0, averaged_shot_data["DATA"].shape[0])
-            self._call_operations_manager(shot_no=f"Average over {shot_data_list}",
+            # COPY X AND Y AXES FROM INDIVIDUAL SHOTS
+            averaged_shot_data["X"] = data_dict[shot_no]["X"]
+            averaged_shot_data["Y"] = data_dict[shot_no]["Y"]
+
+            print("Averaged shot data: ", averaged_shot_data)
+            print("Calling operations manager on the averaged shot data ...\n")
+            self._call_operations_manager(shot_no=f"Average over {shot_nos}",
                                           shot_data=averaged_shot_data,
                                           LABEL=LABEL)
 
 
     
     def _call_operations_manager(self, shot_no, shot_data, LABEL):
+        """Helper function to wrap up the operations manager clauses in the .run() method.
         
+        Parameters
+        ----------
+            shot_no : int
+                The shot number whose data we are processing
+            shot_data : np.ndarray
+                The shot data itself in processed form.
+            LABEL : str
+                Extra detail about the nature of processing which the data has undergone.
+        """
+        
+        # INITIALIZE THE CORRECT OPERATIONS MANAGER USING THE MANAGER_KEY DICTIONARY
         operations_manager = self.manager_key[self.input["DEVICE_TYPE"]][self.input["DEVICE_SPECIES"]](
             DEVICE_NAME=self.input["DEVICE_NAME"],
             shot_no=shot_no,
             label=LABEL,
             shot_data=shot_data) 
+        
+        # CHECK TO SEE WHICH OPERATIONS WE WANT TO GET THE OPERATIONS MANAGER TO PERFORM
+        # LINEOUTS
+        
         if self.operations["LINEOUT"]:
-            if self.input["DEVICE_TYPE"] == "CAMERA":
-                operations_manager.lineouts(axis=self.operations["LINEOUT_AXIS"], ft_interp=self.operations["LINEOUT_FT_INTERP"])
-            else:
-                raise NotImplementedError("Warning: no lineout method provided for probes!")
+                print("Lineout ... \n")
+                if self.input["DEVICE_TYPE"] == "CAMERA":
+                    operations_manager.lineouts(axis=self.operations["LINEOUT_AXIS"], ft_interp=self.operations["LINEOUT_FT_INTERP"])
+                else:
+                    raise NotImplementedError("Warning: no lineout method provided for probes!")
+        # PLOTTING
+        
         if self.operations["PLOT"]:
-            operations_manager.plot()
-
+            print("Plot .. \n")
+            #operations_manager.plot()
+        # CHROMOX FITTING, IF THE CAMERA IS IMAGING CHROMOX
+        
         if self.input["DEVICE_SPECIES"] == "DIGICAM":
             if self.operations["CHROMOX_FIT"]:
+                print("CHROMOX fit ... \n")
                 operations_manager.chromox_fit()
             
         