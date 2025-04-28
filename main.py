7f7cefd5f5985f2a2cc7fd50f4be1b34654260b0
Hayden Ramm <hayden.r.ramm@gmail.com>
2025-04-28 16:21:39 +0200

    moved everything into the main.py file... a bit messy but will have to do for now.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #MODULE IMPORTS
 import sys
 import json
+import os
 sys.path.append(".")
 
 from utils.runmanager import RunManager
@@ -9,6 +10,34 @@ def main(json_path="./input.json"):
     
     with open(json_path) as jsfile:
         input = json.load(jsfile)
+
+    # Parent data path
+    data_path = '/eos/project/h/hiradmat/HRMT Experiments/2024/FIREBALL2/FB2_HRMT64_repository/HRMT64 data'
+
+    # Check to make sure that the parent data path actually exists.
+    if not os.path.exists(data_path):
+        raise NotImplementedError("Error: parent data_path doesn't exist.")
+
+    # Path to shot log
+    shotlog_path = "./example_data/shotlog.csv"
+
+    if not os.path.exists(shotlog_path):
+        raise NotImplementedError("Error: path to shot log not found.")
+
+    # List of subdirectories in parent data path which should
+    folders = ["SCOPE1", "SCOPE2", "EP_Spectrometer_Cams", "PlasmaCell_Cams"]
+
+    # Make sure all of the folders actually exist.
+    for folder in folders:
+        new_path = os.path.join(data_path, folder)
+        if not os.path.exists(new_path):
+            raise NotImplementedError(f"Path {new_path} to folder {folder} does not exist.\n")
+    # Template file names at which the data will be stored
+    template_fnames = {
+        "DIGICAM": 'HRM{}.DigiCam_OD{}_{}_{}.csv',
+        "PROBE": 'C{}_SCOPE{}_{}.csv'
+    }
+
 
 
     # SOMEHOW BUILD THE DATA PATHS DICT
