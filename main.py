6d0cbc18fa98f1d0a9eccafc0a2a984c8cb322f1
Hayden Ramm <hayden.r.ramm@gmail.com>
2025-04-28 17:37:15 +0200

    merged with dictmanager, and then commented everything out ;/ messy, but I don't want to be plagued by merge conflicts.

--- main.py
+++ main.py
@@ -5,34 +5,67 @@ import os
 sys.path.append(".")
 
 from utils.runmanager import RunManager
-from utils.dictmanager import DictManager
+
+# IMPLEMENT LATER...
+#from utils.dictmanager import DictManager
 
 def main(json_path="./input.json"):
     
     with open(json_path) as jsfile:
         input = json.load(jsfile)
 
-    # Need to load the data for all shots, including the background shots.
-    all_shots = input["EXP_SHOT_NOS"] + input["BKG_SHOT_NOS"]
+    #######################################
+    # DICTIONARY MANAGER- IMPLEMENT LATER #
+    #######################################
 
-    # START THE DICTIONARY MANAGER TO CREATE DATA PATH DICTIONARIES
-    dict_manager = DictManager(
-        shot_nos=all_shots,
-        data_path=input["DATA_PATH"],
-        folders=input["DEVICE_FOLDERS"],
-        template_fnames=input["TEMPLATE_FNAMES"], 
-        shotlog_path=input["SHOTLOG_PATH"]
-        )
+    # # Need to load the data for all shots, including the background shots.
+    # all_shots = input["EXP_SHOT_NOS"] + input["BKG_SHOT_NOS"]
+
+    # # START THE DICTIONARY MANAGER TO CREATE DATA PATH DICTIONARIES
+    # dict_manager = DictManager(
+    #     shot_nos=all_shots,
+    #     data_path=input["DATA_PATH"],
+    #     folders=input["DEVICE_FOLDERS"],
+    #     template_fnames=input["TEMPLATE_FNAMES"], 
+    #     shotlog_path=input["SHOTLOG_PATH"]
+    #     )
     
-    # LOAD ALL DATA PATH DICTIONARIES
-    # of form {"DEVICE NAME" : {SHOT NO : /path/to/data}}
-    dict_of_dicts = dict_manager.run()
+    # # LOAD ALL DATA PATH DICTIONARIES
+    # # of form {"DEVICE NAME" : {SHOT NO : /path/to/data}}
+    # dict_of_dicts = dict_manager.run()
 
-    # START THE RUNMANAGER TO PERFORM DATA LOADING AND ARITHMETIC
-    print("Starting run manager ... \n")
+    # # START THE RUNMANAGER TO PERFORM DATA LOADING AND ARITHMETIC
+    # print("Starting run manager ... \n")
+    # run_manager = RunManager(
+    #     input=input,
+    #     data_paths_dict=dict_of_dicts["DEVICE_NAME"]
+    # )
+    
+    ###############################################################
+    ###############################################################
+
+    digicam3_paths_dict = {
+        1:"./example_data/data/BG_HRM3.DigiCam_OD0_1714407435191489_1714407428535000.csv",
+        2:"./example_data/data/HRM3.DigiCam_OD0_1714383312791697_1714383306135000.csv",
+        3:"./example_data/data/HRM3.DigiCam_OD1_1714604587992043_1714604581335000.csv",
+        4:"./example_data/data/BG_HRM3.DigiCam_OD0_1714407435191489_1714407428535000.csv",
+    }
+
+    andor_paths_dict = {
+        1:'./example_data/data/andor.asc'
+    }
+
+    orca_paths_dict = {
+        1:'./example_data/data/1ns_test.dac'
+    }
+
+    probe_paths_dict = {
+        1:"./example_data/data/C1--XX_SCOPE2--00081.csv"
+    }
+
     run_manager = RunManager(
         input=input,
-        data_paths_dict=dict_of_dicts["DEVICE_NAME"]
+        data_paths_dict=orca_paths_dict
     )
 
     run_manager.run()
