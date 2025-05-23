1625abce2ba0c2220a8a6a332d19687afdd62690
Hayden Ramm <hayden.r.ramm@gmail.com>
2025-05-23 11:21:51 +0200

    starting to make 'scope plotting a lot drier.

--- utils/opmanager/probeoperationsmanager.py
+++ utils/opmanager/probeoperationsmanager.py
@@ -14,22 +14,34 @@ class ProbeOperationsManager(OperationsManager):
         super().__init__(DEVICE_NAME, shot_no, label, shot_data, input, std_data)
 
     def plot(self, norm:bool=False):
-        """Plots the four-channel voltage data from the 'scope readout as a function of time."""
+        """Plots the four-channel voltage data from the 'scope readout as a function of time. This will be two 2x2 grids of plots, with each row
+        containing the real-space voltage vs. time data in the lefthand column, and the fourier transform of the data in the right column.
+        
+        Parameters
+        ----------
+            norm : bool = False
+                Whether or not we want to normalize the 
+        """
+
+        #2x2 grid of plots.
+        nrows = 2
+        ncols = 2
+        figsize = (16, 9)
 
         ########################
         # CHANNELS ONE AND TWO #
         ########################
 
-        fig1, axs1 = plt.subplots(nrows=2, ncols=2, figsize=(16,9))
+        fig1, axs1 = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
 
-        # time information- INCLUDING number of discrete time steps and the interval for each step
-        times = self.shot_data["DATA"]["TIMES"]["TIMES"]
-        N = self.shot_data["DATA"]["TIMES"]["N"]
-        dt = self.shot_data["DATA"]["TIMES"]["dt"]
+        # TIME information- INCLUDING number of discrete time steps and the interval for each step
+        times, N, dt = self._get_time_data()
 
-        # voltage information
+        # VOLTAGE information
         channel1_voltages = self.shot_data["DATA"]["VOLTAGES"]["1"]
         channel2_voltages = self.shot_data["DATA"]["VOLTAGES"]["2"]
+        channel3_voltages = self.shot_data["DATA"]["VOLTAGES"]["3"]
+        channel4_voltages = self.shot_data["DATA"]["VOLTAGES"]["4"]
         
         # CHANNEL 1 #
 
@@ -68,10 +80,6 @@ class ProbeOperationsManager(OperationsManager):
         #############
 
         fig2, axs2 = plt.subplots(nrows=2, ncols=2, figsize=(16,9))
-
-        # voltage information
-        channel3_voltages = self.shot_data["DATA"]["VOLTAGES"]["3"]
-        channel4_voltages = self.shot_data["DATA"]["VOLTAGES"]["4"]
         
         # CHANNEL 3 #
 
@@ -104,6 +112,53 @@ class ProbeOperationsManager(OperationsManager):
         fig2.suptitle("Data from Scope")
         fig2.tight_layout()
         plt.show()
+
+    
+    def _get_time_data(self)->tuple[np.ndarray, int, float]:
+        """Returns time data (timestamps, number of samples, time interval between samples) from shot data.
+        
+        Returns
+        -------
+            times : np.ndarray
+                The timestamps of the data (i.e. the points that will be along the "time" axis) from the scope trace.
+            N : int
+                The number of samples over which data from the 'scopes was taken.
+            dt : float
+                The size of the timesteps (intervals between samples), in seconds.
+        """
+
+        times = self.shot_data["DATA"]["TIMES"]["TIMES"]
+        N = self.shot_data["DATA"]["TIMES"]["N"]
+        dt = self.shot_data["DATA"]["TIMES"]["dt"]
+
+        return times, N, dt
+    
+    
+    def _get_voltage_data(self, channel_no:str)->np.ndarray:
+        """Returns voltage data for a given channel on the 'scope. The time data will of course be in array form.
+        
+        Parameters
+        ----------
+            channel_no : str
+                The channel number for which we want voltage data. This should be in STRING format.
+
+        Returns
+        -------
+            channel_voltages : np.ndarray
+                The voltage information for the channel, in array format.
+        """
+
+        # Check to make sure that the channel number is passed as a string.
+        if type(channel_no) != str:
+            raise TypeError("Error: channel number must be passed as a string.")
+
+        # Check to make sure that the provided channel number is between 1 and 4.
+        if channel_no not in ["1", "2", "3", "4"]:
+            raise ValueError("Error: channel number provided is not between 1 and 4.")
+
+        channel_voltages = self.shot_data["DATA"]["VOLTAGES"][channel_no]
+
+        return channel_voltages
 
 
         
